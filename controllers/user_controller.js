6766e0b2cb31000bff4709ea2250cde46695d6ef
annanadd <ananonadd@gmail.com>
2022-03-09 16:12:10 -0500

    user auth

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,42 +1,191 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
+const bcrypt = require("bcrypt");
 const { User } = require("../models");
 
+const jwt = require('jsonwebtoken');
+const auth = require('../middleware/auth');
 
 
-router.get("/", async (req, res) => {
+
+
+/* Register */
+router.post('/register', async (req, res) => {
     try {
-    res.json(await User.find({}));
-    } catch (error) {
-    
-    res.status(400).json(error);
-    }
-});
 
-router.post("/", async (req, res, next) => {
-    try {
-    const user = await User.create(req.body);
-    return res.status(200).json(list)
-    } catch (error) {
-    res.status(400).json(error);
-    }
-});
+    let { username, email, password, passwordCheck } = req.body;
 
-
-router.put("/:id", async (req, res) => {
-    try {
-        res.json(
-            await User.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    if (!email || !password || !passwordCheck) {
+        return res.status(400).json(
+        { 
+            message: 'Missing fields; all fields are required' 
+        }
         );
-    } catch (error) {
-        res.status(400).json(error);
+    }
+
+    if (password.length < 8) {
+        return res.status(400).json(
+        {
+            message: 'Password must be at least 8 characters',
+        }
+        );
+    }
+
+    if (password !== passwordCheck) {
+        return res.status(400).json(
+        { 
+            message: 'Passwords do not match', 
+        }
+        );
+    }
+
+    const existingUser = await User.findOne({ email: email });
+
+    if (existingUser) {
+        return res.status(400).json(
+        { 
+            message: 'Email is already associated with an account',
+        }
+        );
+    }
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+    const newUser = new User({
+
+        username,
+        email, 
+        password: passwordHash,
+
+    });
+
+    const savedUser = await newUser.save();
+
+    res.json(savedUser);
+
+    } catch (err) {
+    res.status(500).json(
+        {
+        error: err.message, 
+        }
+    );
     }
 });
 
-router.delete("/:id", async (req, res) => {
+/* Login */
+router.post('/login', async (req, res) => {
     try {
-        res.json(await User.findByIdAndRemove(req.params.id));
-    } catch (error) {
-        res.status(400).json(error)
+    
+    const { email, password } = req.body;
+
+    if(!email || !password) {
+        return res.status(400).json(
+        {
+            message: 'All fields are required',
+        }
+        );
     }
-})
+
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+        return res.status(400).json(
+        {
+            message: 'Account does not exist; please register',
+        }
+        );
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        return res.status(400).json(
+        {
+            message: 'Invalid login credentials'
+        }
+        );
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    res.json(
+        {
+        token,
+        user: {
+            id: user._id
+        },
+        username: user.username,
+        },
+    );
+
+    } catch (err) {
+    res.status(500).json(
+        {
+        error: err.message,
+        }
+    );
+    }
+});
+
+/* Verify JWT Validity */
+router.post('/tokenIsValid', async (req, res) => {
+    try {
+
+    const token = req.header('x-auth-token');
+    if (!token) {
+        return res.json(false);
+    }
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!verified) {
+        return res.json(false);
+    }
+
+    const user = await User.findById(verified.id);
+
+    if (!user) {
+            return res.json(false);
+    }
+
+    return res.json(true);
+
+    } catch (err) {
+    res.status(500).json(
+        {
+        error: err.message,
+        }
+    );
+    }
+});
+
+/* Get User */
+router.get('/', auth, async (req, res) => {
+
+    const user = await User.findById(req.user);
+
+    res.json(
+    {
+        username: user.username,
+        id: user._id,
+    }
+    );
+});
+
+/* Delete User */
+router.delete('/delete', auth, async (req, res) => {
+    try {
+    
+    const deletedUser = await User.findByIdAndDelete(req.user);
+    res.json(deletedUser);
+
+    } catch (err) {
+
+    res.status(500).json(
+        {
+        error: err.message,
+        }
+    );
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,24 +5,18 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const morgan = require("morgan");
-const controller = require('./controllers/index');
+const controllers = require('./controllers/index');
 
 
-// mongoose.connect(MONGODB_URL, {
-//     useUnifiedTopology: true,
-//     useNewUrlParser: true,
-// });
-
-// mongoose.connection
-//     .on("open", () => console.log("Your are connected to mongoose"))
-//     .on("close", () => console.log("Your are disconnected from mongoose"))
-//     .on("error", (error) => console.log(error));
 
 
-    
+//middleware  
     app.use(cors());
     app.use(morgan("dev")); 
     app.use(express.json()); 
-    app.use('/list', controller.list);
+    app.use('/list', controllers.list);
+    app.use('/', controllers.user);
+
+
 
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
