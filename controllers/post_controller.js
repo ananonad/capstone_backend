780f3dc4d9ef34c4c31dda0977299d3cd23492e6
annanadd <ananonadd@gmail.com>
2022-03-14 16:44:15 -0400

    post updates

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -15,10 +15,10 @@ router.get("/", async (req, res) => {
 
 router.post("/", async (req, res, next) => {
     try {
-    const post = await Post.create(req.body);
-    return res.status(200).json(list)
+        const post = await Post.create(req.body);
+        return res.status(200).json(list)
     } catch (error) {
-    res.status(400).json(error);
+        res.status(400).json(error);
     }
 });
 
@@ -27,18 +27,25 @@ router.put("/:id", async (req, res) => {
     try {
         res.json(
             await Post.findByIdAndUpdate(req.params.id, req.body, { new: true })
-        );
-    } catch (error) {
-        res.status(400).json(error);
-    }
-});
-
-router.delete("/:id", async (req, res) => {
-    try {
-        res.json(await Post.findByIdAndRemove(req.params.id));
-    } catch (error) {
-        res.status(400).json(error)
-    }
-})
-
-module.exports = router
+            );
+        } catch (error) {
+            res.status(400).json(error);
+        }
+    });
+    
+    router.delete("/:id", async (req, res) => {
+        try {
+            res.json(await Post.findByIdAndRemove(req.params.id));
+        } catch (error) {
+            res.status(400).json(error)
+        }
+    })
+    
+    router.get("/:id", async (req, res) => {
+        try {
+            res.json(await Post.findOne({"_id": req.params.id})); 
+        } catch (error) {
+            res.status(400).json(error)
+        }
+    });
+    module.exports = router
